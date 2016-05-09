55fe0a93501aad70c2ea523bbaef80088d2ab948
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2016-05-09 17:44:56 +1000

    Done layout for first menu. Playing with the Popup Demo and trying to integrate it

--- itemsForHireMenu.py
+++ itemsForHireMenu.py
@@ -1,9 +1,12 @@
 from kivy.app import App
 from kivy.app import Builder
-
+from kivy.uix.screenmanager import ScreenManager, Screen
 class itemsForHire(App):
     def build(self):
         self.title = "Items For hire v2"
         self.root = Builder.load_file('menu.kv')
         return self.root
+
+    def hireItem(self):
+        self.root = Builder.load_file('addItemMenu.kv')
 itemsForHire().run()
